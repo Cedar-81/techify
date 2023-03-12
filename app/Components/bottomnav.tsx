6ad769d38f1d880f6bc5defc17505ae17acb748a
Cedar-81 <64336732+Cedar-81@users.noreply.github.com>
2023-03-11 20:27:40 -1100

    modified eventcard and bottomnav styles

--- app/Components/bottomnav.tsx
+++ app/Components/bottomnav.tsx
@@ -1,34 +1,34 @@
-import { FaBolt } from "react-icons/fa";
+import { TfiBolt } from "react-icons/tfi";
 import { MdAdd } from "react-icons/md";
-import { MdOutlineFavorite } from "react-icons/md";
-import { HiTicket } from "react-icons/hi";
+import { MdOutlineFavoriteBorder } from "react-icons/md";
 import { BiSearch } from "react-icons/bi";
+import { BsTicket } from "react-icons/bs";
 
 const Bottomnav = () => {
   return (
-    <div className="fixed md:hidden z-20 shadow-xl bottom-0 w-[100vw] bg-white px-4 rounded-t-xl border-2 border-brand">
+    <div className="fixed md:hidden h-[4.5rem] border-b-0 z-20 shadow-xl bottom-0 w-[100vw] bg-white px-4 rounded-t-xl border-2 border-brand">
       <ul className="flex relative justify-between w-full text-xs py-3">
-        <li className="flex flex-col items-center ">
-          <FaBolt className="icon" />
-          <span className="">Events</span>
+        <li className="flex space-y-1 flex-col items-center ">
+          <TfiBolt className="icon" />
+          <span className="font-medium">Events</span>
         </li>
-        <li className="flex flex-col items-center ">
+        <li className="flex space-y-1 flex-col items-center ">
           <BiSearch className="icon" />
-          <span className="">Search</span>
+          <span className="font-medium">Search</span>
         </li>
-        <li className="flex flex-col items-center relative">
+        <li className="flex space-y-1 flex-col items-center relative">
           <div className="absolute bg-white p-6 border-2 border-brand bottom-3 rounded-full">
             <MdAdd className="icon text-brand" />
           </div>
-          <span className="">Add</span>
+          <span className="font-medium">Add</span>
         </li>
-        <li className="flex flex-col items-center ">
-          <HiTicket className="icon" />
-          <span className="">Tickets</span>
+        <li className="flex space-y-1 flex-col items-center ">
+          <BsTicket className="icon" />
+          <span className="font-medium">Tickets</span>
         </li>
-        <li className="flex flex-col items-center ">
-          <MdOutlineFavorite className="icon" />
-          <span className="">Favourites</span>
+        <li className="flex space-y-1 flex-col items-center ">
+          <MdOutlineFavoriteBorder className="icon" />
+          <span className="font-medium">Favourites</span>
         </li>
       </ul>
     </div>
--- app/Components/eventcard.tsx
+++ app/Components/eventcard.tsx
@@ -6,9 +6,9 @@ import Image from "next/image";
 
 const Eventcard = () => {
   return (
-    <div className="relative md:space-y-6 md:shadow-md cursor-pointer w-full md:pb-0 md:w-[35rem] md:h-[23rem] md:min-h-[20rem] md:flex pb-6 bg-white overflow-hidden min-h-[25rem] shadow-sm rounded-xl border-2 space-y-8">
+    <div className="relative md:w-[25rem] md:space-y-6 md:shadow-md cursor-pointer w-full xl:pb-0 xl:w-[30rem] xl:h-[21rem] xl:min-h-[20rem] xl:flex pb-6 bg-white overflow-hidden min-h-[25rem] shadow-sm rounded-xl border-2 space-y-8">
       {/* CARD IMAGE */}
-      <div className="relative w-full h-[7rem] flex md:h-full md:min-w-[35%]">
+      <div className="relative w-full h-[7rem] flex xl:h-full xl:min-w-[35%]">
         <Image
           src="/assets/cardimg.png"
           alt=""
@@ -17,10 +17,11 @@ const Eventcard = () => {
         />
       </div>
 
-      {/* DETAILS */}
-      <div className="flex flex-col md:space-y-4 space-y-3 px-4 md:pl-6">
-        {/* AUTHOR DETAILS */}
-        <div className="flex justify-end items-center gap-2">
+      <div className="space-y-6">
+        {/* DETAILS */}
+        <div className="flex flex-col md:space-y-4 space-y-3 px-4 md:pl-6">
+          {/* AUTHOR DETAILS */}
+          {/* <div className="flex justify-end items-center gap-2">
           <div className="text-right">
             <p className="text-sm font-medium">Ricky Suare</p>
             <p className="text-xs">DevRel @Nextjs. Web Deve...</p>
@@ -33,44 +34,45 @@ const Eventcard = () => {
               className=" object-cover "
             />
           </div>
-        </div>
+        </div> */}
 
-        {/* CARD DETAILS */}
-        {/* CARD HEADER */}
-        <div className="space-y-1">
-          <h3 className="text-xl font-medium">Nextjs Cohort 2.0</h3>
-          <h4 className="uppercase flex gap-2 items-center text-xs font-medium">
-            online <div className="dot"></div> tue, mar 6th{" "}
-            <div className="dot"></div> 9:00pm
-          </h4>
-        </div>
+          {/* CARD DETAILS */}
+          {/* CARD HEADER */}
+          <div className="space-y-1">
+            <h3 className="text-xl font-medium">Nextjs Cohort 2.0</h3>
+            <h4 className="uppercase flex gap-2 items-center text-xs font-medium">
+              online <div className="dot"></div> tue, mar 6th{" "}
+              <div className="dot"></div> 9:00pm
+            </h4>
+          </div>
 
-        {/* PARAGRAPH AND BOOKING DETAILS */}
-        <div className="space-y-2">
-          <p className="text-sm md:text-xs">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat.
-          </p>
-          <div className="text-sm md:text-xs">
-            <p className="flex items-center gap-2">
-              <RiMoneyDollarCircleLine className="" />
-              <span className="">$13.00</span>
+          {/* PARAGRAPH AND BOOKING DETAILS */}
+          <div className="space-y-2">
+            <p className="text-sm md:text-xs">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat.
             </p>
-            <p className="flex items-center gap-2">
-              <IoPeopleOutline className="" />
-              <span className="">150 participants</span>
-            </p>
-            <p className="flex items-center gap-2">
-              <GoLocation className="" />
-              <span className="">ZOOM</span>
-            </p>
+            <div className="text-sm md:text-xs">
+              <p className="flex items-center gap-2">
+                <RiMoneyDollarCircleLine className="" />
+                <span className="">$13.00</span>
+              </p>
+              <p className="flex items-center gap-2">
+                <IoPeopleOutline className="" />
+                <span className="">150 participants</span>
+              </p>
+              <p className="flex items-center gap-2">
+                <GoLocation className="" />
+                <span className="">ZOOM</span>
+              </p>
+            </div>
           </div>
         </div>
 
         {/* CALL TO ACTION */}
-        <div className="flex items-center gap-2 md:mt-8">
+        <div className="flex items-center gap-2 md:mt-8 px-4">
           <button className="btn border-2 border-gray-800 md:text-sm">
             Book Now
           </button>
